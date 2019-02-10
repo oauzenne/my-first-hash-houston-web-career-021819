26c73f87d41a483f2b76a0d1ca3ef951cb453231
Olivia Auzenne <oauzenne@gmail.com>
2019-02-10 04:51:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,8 +23,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets", "gun powder"]= 2, 4
-
+ shipping_manifest["muskets"]= 2
+ shipping_manifest["gun powder"]= 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
